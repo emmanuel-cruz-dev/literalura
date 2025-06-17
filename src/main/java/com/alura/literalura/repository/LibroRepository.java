3b2960057ffcba6074efaa25fb779e4e05343041
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-17 12:39:41 -0300

    adds and configures LibroRepository

---
+++ src/main/java/com/alura/literalura/repository/LibroRepository.java
@@ -0,0 +1,15 @@
+package com.alura.literalura.repository;
+
+import com.alura.literalura.model.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LibroRepository extends JpaRepository<Libro, Long> {
+    @Query(value = "SELECT * FROM libros WHERE :idioma = ANY (idiomas)", nativeQuery = true)
+    List<Libro> buscarPorIdioma(String idioma);
+
+    Optional<Libro> findByTituloContainingIgnoreCase(String tituloDelLibro);
+}
