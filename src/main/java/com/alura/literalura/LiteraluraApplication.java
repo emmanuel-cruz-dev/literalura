97ed3708b2ff3d016e440e0d8aece410d40b186a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-17 12:36:54 -0300

    adds LibroRepository and AutorRepository in LiteraluraApplication

--- src/main/java/com/alura/literalura/LiteraluraApplication.java
+++ src/main/java/com/alura/literalura/LiteraluraApplication.java
@@ -1,6 +1,9 @@
 package com.alura.literalura;
 
 import com.alura.literalura.principal.Principal;
+import com.alura.literalura.repository.AutorRepository;
+import com.alura.literalura.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,13 +11,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner {
 
+	@Autowired
+	private LibroRepository libroRepository;
+
+	@Autowired
+	AutorRepository autorRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Principal principal = new Principal();
-		principal.showsMenu();
+		Principal principal = new Principal(libroRepository, autorRepository);
+		principal.mostrarMenu();
 	}
 }
