32c5be1faaf7ca3d71d6905feaa215d8bf4d709c
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-17 12:37:25 -0300

    updates properties in record DatosLibro

--- src/main/java/com/alura/literalura/model/DatosLibro.java
+++ src/main/java/com/alura/literalura/model/DatosLibro.java
@@ -6,10 +6,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DatosLibros(
+public record DatosLibro(
+        @JsonAlias("id") Integer idLibro,
         @JsonAlias("title") String titulo,
-        @JsonAlias("authors") List<DatosAutor> autor,
+        @JsonAlias("authors") List<DatosAutor> autores,
         @JsonAlias("languages") List<String> idiomas,
-        @JsonAlias("download_count") Double numeroDeDescargas
+        @JsonAlias("download_count") Integer numeroDeDescargas
 ) {
 }
